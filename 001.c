349d1f8d2fa3acabf69bcc8324291d1c0c12bf7f
Nserein <140895210+Nserein@users.noreply.github.com>
2023-11-07 17:22:06 +0800

    Add files via upload

---
+++ 001.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+//包含一个叫stdio.h的文件
+//std-标准 standard input output
+int main()//主函数-程序的入口-main函数有且仅有一个
+{
+	//double d = 5.11;
+	//printf("%lf\n", d);；    可以用lf也可以用f，但一般double用lf，float用f
+	//int ske = 55;
+	//printf("%d\n", ske);    %d----打印整型十进制数据
+	// 
+	// 
+	// 
+	//char aa = 'A';
+	//printf("%c\n", aa);     %c——打印字符格式的数据
+	// 
+	// 
+	//这里完成任务
+	//在屏幕上输出hello world
+	//函数-print function-printf-打印函数
+	//库函数-c语言本身提供给我们使用的函数
+	// 
+	// 
+    //printf("Hello World\n");
+	printf("%d\n", sizeof(char));
+	return 0;//返回0
+}
+//int整型
+//main前面的int表示main调用返回一个整型值
+// 
+// 
+// 
+// %d----打印十进制数字
+// %c——打印字符
+// %p——打印地址
+// %f——打印浮点数字——小数
+// %x——打印十六进制数字
+// 
+// 
+//char  字符数据类型
+//short 短整型
+//int 整型
+//long 长整型
+//long long 更长的整型
+//float 单精度浮动点数
+//double 双精度浮动点数
